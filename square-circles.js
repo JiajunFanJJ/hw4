3d0dec8fb74680b0bedfa15294c2b9aec6e270a8
JiajunFanJJ <55294567+JiajunFanJJ@users.noreply.github.com>
2019-10-07 16:09:21 -0700

    Update square-circles.js

--- square-circles.js
+++ square-circles.js
@@ -14,7 +14,7 @@ function draw() {
   rectMode(CENTER);
 
   for (var i = 0; i < x.length; i = i + 10) {
-    rect(x[i], y[i], 1 + (x.length - i), 1 + (x.length - i));
+    rect(x[i], y[i], 1 + (x.length - i), 1 + (x.length - (i * 2)));
   }
 
   x = x.slice(-50); // remove all but the last 50 x values
