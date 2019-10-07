2664cbac2e405cdc278a3da5d866c5200d175416
JiajunFanJJ <55294567+JiajunFanJJ@users.noreply.github.com>
2019-10-07 16:32:09 -0700

    Update faster-points.js

--- faster-points.js
+++ faster-points.js
@@ -1,13 +1,13 @@
 function setup() {
   createCanvas(400, 400);
-  loop(1000);
   colorMode(HSB, width, height, 100);
 }
 
 function draw() {
-  var y = random(height);
+  
 
   for (var x = random(width); x < width; x++) {
+    var y = random(height);
     stroke(x, y, 100);
     point(x, y);
   }
